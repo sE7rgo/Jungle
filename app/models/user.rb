acc528c57cc761ee858a1c9d87e7ce0e11f517d7
sE7rgo <se7rgo@gmail.com>
2020-08-25 22:42:53 +0000

    defined self.authenticate_with_credentials method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+    user = self.find_by_email(email.strip.downcase)
+    if user
+      user.authenticate(password)
+    end
+  end
+
+
 end
